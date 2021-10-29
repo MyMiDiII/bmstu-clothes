949def3b84dc5125c181bd0c44c99d39538f0e82
MyMiDiII <mymidi.student@yandex.ru>
2021-10-29 14:26:53 +0300

    FIX: only base of mygl class

--- src/mygl.py
+++ src/mygl.py
@@ -2,6 +2,7 @@ from PyQt5 import QtGui
 from PyQt5 import QtOpenGL
 
 import OpenGL.GL as gl
+from OpenGL import GLU
 
 from OpenGL.arrays import vbo
 import numpy as np
@@ -12,43 +13,16 @@ class myGL(QtOpenGL.QGLWidget):
         QtOpenGL.QGLWidget.__init__(self, parent)
 
     def initializeGL(self):
-        self.qglClearColor(QtGui.QColor(0, 0, 255))
-        gl.glEnable(gl.GL_DEPTH_TEST)
+        self.qglClearColor(QtGui.QColor(0, 0, 0))
 
-        self.initGeometry()
-
-    def initGeometry(self):
-        self.cubeVtxArray = np.array(
-            [[0.0, 0.0, 0.0],
-             [1.0, 0.0, 0.0],
-             [1.0, 1.0, 0.0],
-             [0.0, 1.0, 0.0],
-             [0.0, 0.0, 1.0],
-             [1.0, 0.0, 1.0],
-             [1.0, 1.0, 1.0],
-             [0.0, 1.0, 1.0]])
-        self.vertVBO = vbo.VBO(np.reshape(self.cubeVtxArray,
-                          (1, -1)).astype(np.float32))
-        self.vertVBO.bind()
+    def resizeGL(self, width, height):
+        pass 
 
     def paintGL(self):
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        GLUT.glutInit(sys.argv)
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+        GLUT.glutWireTeapot(0.1)
+        gl.glFlush()
 
-        gl.glPushMatrix()    # push the current matrix to the current stack
-
-        gl.glTranslate(0.0, 0.0, -50.0)    # third, translate cube to specified depth
-        gl.glScale(20.0, 20.0, 20.0)       # second, scale cube
-        gl.glTranslate(-0.5, -0.5, -0.5)   # first, translate cube center to origin
-
-        gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
-        gl.glEnableClientState(gl.GL_COLOR_ARRAY)
-
-        gl.glVertexPointer(3, gl.GL_FLOAT, 0, self.vertVBO)
-        #gl.glColorPointer(3, gl.GL_FLOAT, 0, self.colorVBO)
-
-        #gl.glDrawElements(gl.GL_QUADS, len(self.cubeIdxArray), gl.GL_UNSIGNED_INT, self.cubeIdxArray)
-
-        gl.glDisableClientState(gl.GL_VERTEX_ARRAY)
-        gl.glDisableClientState(gl.GL_COLOR_ARRAY)
-
-        gl.glPopMatrix()    # restore the previous modelview matrix
+    def initGeometry(self):
+        pass
